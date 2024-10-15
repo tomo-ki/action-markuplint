f38446b41e9dbfde67b3cb467e4f1c717ffb2c7a
tomoki-hara <tomoki_hara@r.recruit.co.jp>
2024-10-15 13:35:29 +0900

    wip

--- markuplint-formatter-rdjson/index.js
+++ markuplint-formatter-rdjson/index.js
@@ -1,7 +1,16 @@
-const { createFormatter } = require("@markuplint/ml-core");
+function convertSeverity(s) {
+  switch (s.toLowerCase()) {
+    case "error":
+      return "ERROR";
+    case "warning":
+      return "WARNING";
+    default:
+      return "INFO";
+  }
+}
 
-module.exports = createFormatter((results) => {
-  const rdjsonResults = results.map((result) => ({
+function processMarkuplintResults(results) {
+  return results.map((result) => ({
     message: result.message,
     location: {
       path: result.filePath,
@@ -12,17 +21,31 @@ module.exports = createFormatter((results) => {
         },
       },
     },
-    severity: result.severity === "error" ? "ERROR" : "WARNING",
+    severity: convertSeverity(result.severity),
     code: {
       value: result.ruleId,
+      url: `https://markuplint.dev/rules/${result.ruleId}`,
     },
+    original_output: JSON.stringify(result),
   }));
+}
 
-  return JSON.stringify({
+let input = "";
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+process.stdin.on("data", function (chunk) {
+  input += chunk;
+});
+
+process.stdin.on("end", function () {
+  const markuplintResults = JSON.parse(input);
+  const rdjsonResults = {
     source: {
       name: "markuplint",
-      url: "https://github.com/markuplint/markuplint",
+      url: "https://markuplint.dev/",
     },
-    diagnostics: rdjsonResults,
-  });
+    diagnostics: processMarkuplintResults(markuplintResults),
+  };
+  console.log(JSON.stringify(rdjsonResults, null, 2));
 });
--- script.sh
+++ script.sh
@@ -1,28 +1,33 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 
 cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
 
+TEMP_PATH="$(mktemp -d)"
+PATH="${TEMP_PATH}:$PATH"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-echo "::group::🐶 Installing reviewdog..."
-curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b ./bin
-echo "::endgroup::"
+echo '::group::🐶 Installing reviewdog ... https://github.com/reviewdog/reviewdog'
+curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b "${TEMP_PATH}" "${REVIEWDOG_VERSION}" 2>&1
+echo '::endgroup::'
 
-echo "::group:: Installing markuplint and formatter..."
-npm install markuplint @markuplint/config-presets
-npm install --save-dev ./markuplint-formatter-rdjson
-echo "::endgroup::"
+if ! type markuplint > /dev/null 2>&1; then
+  echo '::group:: Running `npm install` to install markuplint ...'
+  npm install
+  echo '::endgroup::'
+fi
 
-echo "::group:: Running markuplint with reviewdog..."
-markuplint ${INPUT_MARKUPLINT_FLAGS} \
-  | ./bin/reviewdog \
-      -f=rdjson \
+echo '::group:: Running markuplint with reviewdog 🐶 ...'
+markuplint -f JSON ${INPUT_MARKUPLINT_FLAGS:-'.'} \
+  | node $GITHUB_ACTION_PATH/markuplint-formatter-rdjson.js \
+  | reviewdog -f=rdjson \
       -name="markuplint" \
-      -reporter="${INPUT_REPORTER}" \
+      -reporter="${INPUT_REPORTER:-github-pr-review}" \
       -filter-mode="${INPUT_FILTER_MODE}" \
       -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-      -level="${INPUT_LEVEL}"
+      -level="${INPUT_LEVEL}" \
+      ${INPUT_REVIEWDOG_FLAGS}
 
 reviewdog_rc=$?
-echo "::endgroup::"
+echo '::endgroup::'
 exit $reviewdog_rc
