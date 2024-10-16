5e1e0cd5f4ff5338a9f4b98f96b47f2be7f90446
tomoki-hara <tomoki_hara@r.recruit.co.jp>
2024-10-16 09:15:14 +0900

    formatter

--- markuplint-formatter-rdjson/index.js
+++ markuplint-formatter-rdjson/index.js
@@ -9,43 +9,34 @@ function convertSeverity(s) {
   }
 }
 
-function processMarkuplintResults(results) {
-  return results.map((result) => ({
-    message: result.message,
-    location: {
-      path: result.filePath,
-      range: {
-        start: {
-          line: result.line,
-          column: result.col,
-        },
-      },
-    },
-    severity: convertSeverity(result.severity),
-    code: {
-      value: result.ruleId,
-      url: `https://markuplint.dev/rules/${result.ruleId}`,
-    },
-    original_output: JSON.stringify(result),
-  }));
-}
-
-let input = "";
-process.stdin.resume();
-process.stdin.setEncoding("utf8");
-
-process.stdin.on("data", function (chunk) {
-  input += chunk;
-});
-
-process.stdin.on("end", function () {
-  const markuplintResults = JSON.parse(input);
-  const rdjsonResults = {
+module.exports = function (results) {
+  const rdjson = {
     source: {
       name: "markuplint",
       url: "https://markuplint.dev/",
     },
-    diagnostics: processMarkuplintResults(markuplintResults),
+    diagnostics: [],
   };
-  console.log(JSON.stringify(rdjsonResults, null, 2));
-});
+
+  results.forEach((result) => {
+    const diagnostic = {
+      message: result.message,
+      location: {
+        path: result.filePath,
+        range: {
+          start: { line: result.line, column: result.col },
+        },
+      },
+      severity: convertSeverity(result.severity),
+      code: {
+        value: result.ruleId,
+        url: `https://markuplint.dev/rules/${result.ruleId}`,
+      },
+      original_output: JSON.stringify(result),
+    };
+
+    rdjson.diagnostics.push(diagnostic);
+  });
+
+  return JSON.stringify(rdjson);
+};
